fc8b054e55b0ff84444362a6d586a07a80448b68
goldie <codegayle@gmail.com>
2023-03-22 20:44:31 -0400

    added source code

---
+++ index.js
@@ -0,0 +1,20 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const markDown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+//use markDown.generateMarkdown() to incorporate data from inquirer to make readme
+
+// TODO: Create a function to initialize app
+//include prompt and writefile
+function init() {}
+
+// Function call to initialize app
+init();
