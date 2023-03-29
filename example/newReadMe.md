ab8591707f1d57572ba5e90f616a97105f4fa88c
goldie <codegayle@gmail.com>
2023-03-28 22:09:06 -0400

    added screenshot of example readme

---
+++ example/newReadMe.md
@@ -0,0 +1,26 @@
+# Readme Generator
+![Apache License](https://img.shields.io/badge/license-Apache-brightgreen)
+## Table of Contents
++ [Description](#description)
++ [Installation](#installation)
++ [Usage](#usage)
++ [Contributions](#contributions)
++ [Test Instructions](#test-instructions)
++ [License](#license)
++ [Questions](#questions)
+## Description
+This is a commandline application to generate a readme for a programming file
+## Installation
+Uses Node v16 and Inquirer npm package
+## Usage
+All the details about your project
+## Contributions
+All code by myself
+## Test Instructions
+N/A
+## License 
+Copyright (C) 2023 @goldengayle Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at [http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0). Apache License Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+## Questions
+You can reach me by email at [gaylelennox@gmail.com](mailto:gaylelennox@gmail.com) or view my other projects on github at [goldengayle](https://github.com/goldengayle).
+
+
--- index.js
+++ index.js
@@ -1,84 +1,117 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const generateMarkDown = require('./utils/generateMarkdown.js');
 const fs = require('fs');
 
 
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a description of your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+    },
+     {
+       type: 'input',
+       name: 'usage',
+       message: 'What information do you need to use this project?',
+     },
+     {
+       type: 'input',
+       name: 'contributions',
+       message: 'What are the contribution guidelines?',
+     },
+     {
+       type: 'input',
+       name: 'test',
+       message: 'What are the test instructions?',
+     },
+     {
+       type: 'list',
+       name: 'license',
+       message: 'What license are you using?',
+       choices: ['MIT', 'Apache', 'GPL_3.0', 'None']
+     },
+     {
+       type: 'input',
+       name: 'github',
+       message: 'What is your github username?',
+     },
+     {
+       type: 'input',
+       name: 'email',
+       message: 'What is your email address?',
+     },
+     {
+        type: 'input',
+        name: 'year',
+        message: 'What is the current year?',
+      }
+]
 
 
-const generateHTML = (response) =>{
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
- ${response}
-</body>
-</html>`;
+
+
+function generateaMarkDown({ title, description, installation, usage, contributions, test, license, github, email, year }) {
+
+if(license === 'MIT'){
+    var licensetext = ' Copyright (C) ' + year + ' @' +github + ' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'
+} else if(license === 'Apache'){
+    var licensetext= 'Copyright (C) ' + year + ' @' +github + ' Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at [http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0). Apache License Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.'
+} else if (license ==="GPL_3.0"){
+    var licensetext =' Copyright (C) ' + year + ' @' +github + ' This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.You should have received a copy of the GNU General Public License along with this program.  If not, see [https://www.gnu.org/licenses](https://www.gnu.org/licenses).'
+} else if (license ==='None'){
+    var licensetext ="This project is not licensed."
 }
 
-// TODO: Create an array of questions for user input
-
-//description, installation instructions, usage information, contribution guidelines, and test instructions
-
-
-const promptUser =() => {
-return inquirer.prompt([
-  {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?',
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Write a description of your project',
-  },
-  {
-    type: 'list',
-    name: 'license',
-    message: 'What license are you using',
-    choices: ['MIT', 'Apache'],
-  },
-  {
-    type: 'input',
-    name: 'github',
-    message: 'What is your github username?',
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'What is your email address',
-  }
-
-])}
+if (license ==="None"){
+  var badge =""}
+  else{
+    var badge = "![" + license +" License](https://img.shields.io/badge/license-"+license+"-brightgreen)"}
+    return `# ${title}
+${badge}
+## Table of Contents
++ [Description](#description)
++ [Installation](#installation)
++ [Usage](#usage)
++ [Contributions](#contributions)
++ [Test Instructions](#test-instructions)
++ [License](#license)
++ [Questions](#questions)
+## Description
+${description}
+## Installation
+${installation}
+## Usage
+${usage}
+## Contributions
+${contributions}
+## Test Instructions
+${test}
+## License 
+${licensetext}
+## Questions
+You can reach me by email at [${email}](mailto:${email}) or view my other projects on github at [${github}](https://github.com/${github}).
 
 
+`
 
 
-/*.then((answers) =>{
+}
 
 
-  fs.writeFile
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-//use markDown.generateMarkdown() to incorporate data from inquirer to make readme
+const init = () => {
+    inquirer.prompt(questions)
+        .then((answers) => fs.writeFile('newReadMe.md', generateaMarkDown(answers), (err) => {
+            err ? console.error(err) : console.log(`response logged!`)
+        }))
+}
 
-// TODO: Create a function to initialize app
-//include prompt and writefile */
-const init=() => {
-  promptUser()
-  .then((answers) => {const readmeFile =   generateMarkDown.generateMarkDown(answers)})
-  .then((writeFile ('log.md', readmeFile, (err) => {
-  
-  err ? console.error(err) : console.log('response logged!')})))}
-
-
-
-
-// Function call to initialize app
-init()
+init();
